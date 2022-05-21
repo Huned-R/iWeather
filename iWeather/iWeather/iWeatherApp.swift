75fee9828816ed096be46797b5a6c20508510dd5
hunedR <hunedrassiwala@gmail.com>
2022-05-21 09:39:17 +0400

    huned/initial_commit

---
+++ iWeather/iWeather/iWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWeatherApp.swift
+//  iWeather
+//
+//  Created by Huned on 21/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct iWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
